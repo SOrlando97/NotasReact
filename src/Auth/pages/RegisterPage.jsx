6ccffe2e16af9e08c2bd24df5bf5f83de14f97cb
Orlando <orlandovanegas16@hotmai.com>
2023-01-18 17:07:30 -0500

    validador registro

--- src/Auth/pages/RegisterPage.jsx
+++ src/Auth/pages/RegisterPage.jsx
@@ -1,17 +1,76 @@
 import {  MDBContainer,  MDBCol,  MDBRow,  MDBBtn,  MDBIcon,  MDBInput,  MDBCheckbox} from 'mdb-react-ui-kit';
+import { useForm } from '../../hooks';
 import { AuthLayout } from "../layout/AuthLayout"
 
 export const RegisterPage = () => {
+
+  const formData = {
+    email : 'orlando@correo.com',
+    password: '123456',
+    displayName: 'Orlando',
+  }
+  
+
+  const formValidations = {
+
+    email : [(value)=>value.includes('@'),'El correo debe tener una @'],
+    password:[(value)=>value.length>= 6,'El password debe tener mas de 6 letras'],
+    displayName: [(value)=>value.length>= 1,'El nombre es obligatorio'],
+
+  }
+
+  const { 
+    displayName,email, password, onInputChange, formState,
+    inFormValid, displayNameValid, emailValid, passwordValid,
+  } =  useForm( formData, formValidations);
+
+  const onSubmit = ( event ) => {
+    
+    event.preventDefault();
+    console.log ( formState );
+
+  }
+  const emailerror = 'a'
   return (
     <>
-      <AuthLayout>
-          <MDBInput wrapperClass='mb-4' label='Nombre'  type='name' size='lg'/>
-          <MDBInput wrapperClass='mb-4' label='Direccion de correo'  type='email' size='lg'/>
-          <MDBInput wrapperClass='mb-4' label='Contraseña'  type='password' size='lg'/>
-              
-          <MDBBtn className="mb-1 w-100">Registrate</MDBBtn>  
-          <a href='/auth/login' >Ya tienes cuenta? Inicia Sesión</a>
-      </AuthLayout>
+      <form onSubmit={ onSubmit }>
+        <AuthLayout>
+              <MDBInput 
+                wrapperClass='mb-4' 
+                label='Nombre'  
+                type='name' 
+                name='displayName'
+                value={ displayName }
+                onChange= { onInputChange }
+                size='lg'/>
+
+                {displayNameValid && <div className='diverror'><h6 className='errorregisterinput'>{displayNameValid}</h6></div>}
+              <MDBInput 
+                wrapperClass='mb-4' 
+                label='Direccion de correo'  
+                type='email' 
+                name='email'
+                value={ email }
+                onChange= { onInputChange }
+                size='lg'/>
+
+                {emailValid && <div className='diverror'><h6 className='errorregisterinput'>{emailValid}</h6></div>}
+              <MDBInput 
+                wrapperClass='mb-4' 
+                label='Contraseña'  
+                type='password' 
+                name='password'
+                value={ password }
+                onChange= { onInputChange }
+                size='lg'/>
+                
+                {passwordValid && <div className='diverror'><h6 className='errorregisterinput'>{passwordValid}</h6></div>}
+                  
+              <MDBBtn type='submit' className="mb-1 w-100">Registrate</MDBBtn>  
+              <a href='/auth/login' >Ya tienes cuenta? Inicia Sesión</a>
+          </AuthLayout>
+      </form>
+      
     </>
   )
 }
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [formValidation, setFormValidation] = useState({})
+    useEffect(() => {
+
+        createValidators();
+
+    }, [formState])
+    
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,10 +23,24 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidators = () =>{
+
+        const formCheckedValues = {};
+        for (const formField of Object.keys( formValidations )) {
+            const [ fn, errorMessage = 'Errores de Validacion.' ] = formValidations[formField];
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+        }
+
+        setFormValidation( formCheckedValues );
+
+    }
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+
+        ...formValidation,
     }
 }
