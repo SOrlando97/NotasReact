bf23f00015d6ceea1a7e65c99b841d02036bf246
Orlando <orlandovanegas16@hotmai.com>
2023-02-18 14:40:34 -0500

    prueba para hacer desploy

---
+++ src/ui/components/Footerpag.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
+
+export const Footerpag =() =>{
+  return (
+    <>
+    <MDBFooter className='text-center text-lg-left footer'>
+      <div className='text-center p-3' >
+        &copy; 2023 Copyright: Stiven Orlando Mendez Vanegas
+
+      </div>
+    </MDBFooter>
+    
+    </>    
+  )
+}
